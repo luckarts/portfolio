2842ae9d86f37c18a735441af06bcbb4e407a5dd
luckarts <luc.bachelerieart@gmail.com>
2020-08-30 00:36:53 +0200

    deploy

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/public/'],
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTestsAfterEnv.js'],
   setupFilesAfterEnv: [
     'testing-library/jest-dom/extend-expect',
     '@testing-library/user-event',
